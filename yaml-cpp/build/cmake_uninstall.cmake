24847936a5aa66ede1a6e20d8996e1201ab667a9
acf-patrick <miharisoap@gmail.com>
2024-02-07 16:27:15 +0300

    updated .gitignores

---
+++ yaml-cpp/build/cmake_uninstall.cmake
@@ -0,0 +1,21 @@
+if(NOT EXISTS "D:/FIT_Apprenti_Vague_006/ECS/libraries/yaml-cpp/build/install_manifest.txt")
+  message(FATAL_ERROR "Cannot find install manifest: D:/FIT_Apprenti_Vague_006/ECS/libraries/yaml-cpp/build/install_manifest.txt")
+endif()
+
+file(READ "D:/FIT_Apprenti_Vague_006/ECS/libraries/yaml-cpp/build/install_manifest.txt" files)
+string(REGEX REPLACE "\n" ";" files "${files}")
+foreach(file ${files})
+  message(STATUS "Uninstalling $ENV{DESTDIR}${file}")
+  if(IS_SYMLINK "$ENV{DESTDIR}${file}" OR EXISTS "$ENV{DESTDIR}${file}")
+    exec_program(
+      "D:/vcpkg/downloads/tools/cmake-3.27.1-windows/cmake-3.27.1-windows-i386/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+      OUTPUT_VARIABLE rm_out
+      RETURN_VALUE rm_retval
+      )
+    if(NOT "${rm_retval}" STREQUAL 0)
+      message(FATAL_ERROR "Problem when removing $ENV{DESTDIR}${file}")
+    endif()
+  else(IS_SYMLINK "$ENV{DESTDIR}${file}" OR EXISTS "$ENV{DESTDIR}${file}")
+    message(STATUS "File $ENV{DESTDIR}${file} does not exist.")
+  endif()
+endforeach()
